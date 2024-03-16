edc23430c36e6fab3aee6193ae5bf1dc7ea787cb
hepromark <markdo28102004@gmail.com>
2024-03-16 17:10:46 -0400

    Added main

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "IntrinsicSolver.h"
+#include "opencv2/core.hpp"
+
+int main () {
+    const cv::Size PATTERN = {7, 5};
+    const double CHESS_SQUARE = 26;  // mm
+    const cv::Size IMAGE_SIZE = {1920, 1080};
+
+    IntrinsicSolver intrinsic_solver = IntrinsicSolver(PATTERN, CHESS_SQUARE, IMAGE_SIZE);
+
+    intrinsic_solver.calibrate("C:\\Users\\markd\\Documents\\GitHub\\StereoVisionDepthMap\\calibrated_txt",
+                               "C:\\Users\\markd\\Documents\\GitHub\\StereoVisionDepthMap\\K1.txt");
+}
