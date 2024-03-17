f49c7b1d0d87a0d19cf9e1cdace4eacb1c2ad309
Miekale Smith <miekale.smith@gmail.com>
2024-03-17 14:35:18 -0400

    trangulate and solve for p

--- Triangulate.cpp
+++ Triangulate.cpp
@@ -1,25 +1,32 @@
-#include <iostream>
 #include <vector>
-
 #include <opencv2/core.hpp>
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
-#include <opencv2/calib3d.hpp>
+#include <iostream>
+#include <cmath>
 
-float BASELINE = 130.0;
+const float BASELINE = 130.0;
 
-cv::Mat calc_P(const cv::Mat &fund1, const cv::Mat& fund2, std::vector<cv::Point> points) {
+cv::Mat calc_P(const cv::Mat &camera1, const cv::Mat& camera2, std::vector<cv::Point> points) {
 
-    cv::Mat system = (points[0].x * fund1.row(2).t() - fund1.row(0).t()).t();
-    system.push_back((points[0].y * fund1.row(2).t() - fund1.row(1).t()).t());
-    system.push_back((points[1].x * fund2.row(2).t() - fund2.row(0).t()).t());
-    system.push_back((points[1].y * fund2.row(2).t() - fund2.row(1).t()).t());
+    cv::Mat system = (points[0].x * camera1.row(2) - camera1.row(0));
+    system.push_back((points[0].y * camera1.row(2) - camera1.row(1)));
+    system.push_back((points[1].x * camera2.row(2) - camera2.row(0)));
+    system.push_back((points[1].y * camera2.row(2) - camera2.row(1)));
+
+    std::cout << system << std::endl;
 
     cv::Mat output = cv::Mat();
 
     cv::SVD::solveZ(system,output);
+
+    output = output / output.at<double>(3,0);
+
     return output;
 }
 
-
+double triangulate(cv::Mat point1, cv::Mat point2) {
+    double x = std::abs(point1.at<double>(0,0) - point2.at<double>(0,0));
+    double y = std::abs(point1.at<double>(1,0) - point2.at<double>(1,0));
+    double z = std::abs(point1.at<double>(2,0) - point2.at<double>(2,0));
+    return std::pow(x*x + y*y + z*z,0.5);
+}
