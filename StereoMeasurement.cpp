69831b05c6bdc7fe6e3be854b920f00c199ee445
ericraze16 <109770798+ericraze16@users.noreply.github.com>
2024-03-17 17:51:25 -0400

    Continuing to build out functionality for automated distance measurement

--- StereoMeasurement.cpp
+++ StereoMeasurement.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include "Distortion.h"
+#include "PointSelection.h"
+#include <fstream>
 
 #include "StereoMeasurement.h"
 
@@ -12,6 +14,52 @@
 
 StereoMeasurement::StereoMeasurement() {
 
+
+    //Read in camera intrinsic parameters (including distortion)
+    std::ifstream fin("C:\\Users\\aleon\\CLionProjects\\Stereo Vision Depth Map\\Calibration\\K1.txt");
+
+    if(!fin) {
+        std::cout << "Could not open file to read intrinsics";
+    }
+
+
+    //Reading intrinsic parameters
+    std::vector<std::vector<double>> data;
+    std::string line;
+
+    //Get 1 line at a time
+    while (std::getline(fin, line)) {
+        std::vector<double> row;
+        std::string value;
+        for (char c : line) {
+
+            //Skip opening brackets
+            if (c == '(' || c == '[') {
+                continue;
+            } else if (c == ',' || c == ';' || c == ')' || c == ']') {
+                if (!value.empty()) {
+                    row.push_back(std::stod(value));
+                    value.clear();
+                }
+            } else {
+                value.push_back(c);
+            }
+        }
+        if (!row.empty()) {
+            data.push_back(row);
+        }
+    }
+
+    // Convert parsed values into matrix
+    left_cam_intrinsics = cv::Mat(data.size(), data[0].size(), CV_64F);
+    for (int i = 0; i < left_cam_intrinsics.rows; ++i) {
+        for (int j = 0; j < left_cam_intrinsics.cols; ++j) {
+            left_cam_intrinsics.at<double>(i, j) = data[i][j];
+        }
+    }
+
+    std::cout<<left_cam_intrinsics;
+
 }
 
 
@@ -19,10 +67,17 @@ void StereoMeasurement::start() {
 
     //Take photos and undistort
     take_photos();
+    left_image_undistorted = Distortion::correct_distortion
+            (left_image_distorted,left_cam_intrinsics, left_cam_distortion);
+    right_image_undistorted = Distortion::correct_distortion(
+            right_image_distorted, right_cam_intrinsics, right_cam_distortion);
 
+    //Get user to select points
+    left_points = PointSelection::getPoints(left_image_undistorted, POINTS_PER_PHOTO);
+    right_points = PointSelection::getPoints(right_image_undistorted, POINTS_PER_PHOTO);
 
-    cv::imshow("Left image", left_image);
-    cv::imshow("Right image", right_image);
+    cv::imshow("Left image", left_image_distorted);
+    cv::imshow("Right image", right_image_distorted);
     while(cv::waitKey(1) != 'q'){}
 
 
@@ -31,10 +86,6 @@ void StereoMeasurement::start() {
     //Show left and right photos to user and get matching points
     //Pass matching points to 3D coordinate finder x2
     //Pass real world points to 3D distance solver
-
-    //Connect to cameras
-    // StereoMeasurement distance_test_1;
-
 
 
 
@@ -50,8 +101,8 @@ void StereoMeasurement::take_photos() {
         std::cout << "Unable to open cameras";
     }
 
-    left_cam >> left_image;
-    right_cam >> right_image;
+    left_cam >> left_image_distorted;
+    right_cam >> right_image_distorted;
 
     left_cam.release();
     right_cam.release();
--- StereoMeasurement.h
+++ StereoMeasurement.h
@@ -20,7 +20,17 @@ public:
 private:
     const int LEFT_CAM_INDEX = 2;
     const int RIGHT_CAM_INDEX = 0;
-    cv::Mat left_image, right_image;
+    const int CAM_MATRIX_DIMENSION = 3;
+    const int POINTS_PER_PHOTO = 3;
+
+    //Matricies to store images
+    cv::Mat left_image_distorted, right_image_distorted, left_image_undistorted, right_image_undistorted;
+
+    //Matricies to store camera parameters
+    cv::Mat left_cam_intrinsics, left_cam_distortion, right_cam_intrinsics, right_cam_distortion;
+
+    //User selected points
+    std::vector<cv::Point> left_points, right_points;
 
     void take_photos();
 
