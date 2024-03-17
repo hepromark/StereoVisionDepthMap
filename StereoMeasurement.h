c617ef29514c5aecbfc1c85fa75ac9b1379df4eb
ericraze16 <109770798+ericraze16@users.noreply.github.com>
2024-03-17 15:12:10 -0400

    Started StereoMeasurement class to incorporate helper classes/function.
    Added functionality to take photos with separate cameras.

---
+++ StereoMeasurement.h
@@ -0,0 +1,26 @@
+//
+// Created by aleon on 2024-03-17.
+//
+
+#include <opencv2/core.hpp>
+
+#ifndef STEREO_VISION_DEPTH_MAP_STEREOMEASUREMENT_H
+#define STEREO_VISION_DEPTH_MAP_STEREOMEASUREMENT_H
+
+class StereoMeasurement {
+
+public:
+
+    StereoMeasurement();
+
+    //std::vector<cv::Mat>
+    static void take_photos();
+
+private:
+
+
+
+};
+
+
+#endif //STEREO_VISION_DEPTH_MAP_STEREOMEASUREMENT_H
